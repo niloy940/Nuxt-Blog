120b440a20c7140830facec16d28f7e7ce202e99
Niloy Quazi <niloy940@gmail.com>
2021-03-29 13:11:58 +0600

    building and deployed to github pages

---
+++ dist/_nuxt/static/1617001845/manifest.js
@@ -0,0 +1,1 @@
+__NUXT_JSONP__("manifest.js", {routes:["\u002Fabout","\u002Fposts","\u002Fadmin\u002Fauth","\u002F","\u002Fposts\u002F-MWWk-vUPrE8wAFzVQ-O","\u002Fposts\u002F-MWWqSJ3ur9lSJtk50rD","\u002Fposts\u002F-MWanRBKrSGQRS0xwE6g","\u002Fposts\u002F-MWWrGduHuuZ_DIQrhCn","\u002Fposts\u002F-MWpsEhjjXvZgj7qH2Zj","\u002Fposts\u002F-MWvD0GJWle1og3TmDsr"]})
--- nuxt.config.js
+++ nuxt.config.js
@@ -70,7 +70,7 @@ export default {
   },
 
   router: {
-    base: '/Nuxt-Blog/'
+    base: '/Nuxt-Blog/dist'
   }
 
 }
