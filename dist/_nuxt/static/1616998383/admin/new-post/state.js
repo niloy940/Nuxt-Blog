dcccfbc01b36382d87c58a52191e56e8ef34fc67
Niloy Quazi <niloy940@gmail.com>
2021-03-29 13:05:13 +0600

    deploying to github

---
+++ dist/_nuxt/static/1616998383/admin/new-post/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1616998383",layout:"admin",error:a,state:a,serverRendered:false,routePath:"\u002Fadmin\u002Fnew-post",config:{app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a}}}}(null));
--- nuxt.config.js
+++ nuxt.config.js
@@ -69,4 +69,8 @@ export default {
     mode: 'out-in'
   },
 
+  router: {
+    base: '/Nuxt-Blog/'
+  }
+
 }
